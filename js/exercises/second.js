4c12501ca14ddaea2ff95fd41ef74c3729e0c481
Nicolas Gongora <nicolasgongora28@gmail.com>
2020-12-31 00:53:26 -0500

    complete 7th exercise

--- js/exercises/second.js
+++ js/exercises/second.js
@@ -30,14 +30,17 @@ const countWords = (str, word) =>
  */
 
 const isPalindrome = (word) =>
-  word.toLowerCase() === word.toLowerCase().split("").reverse().join("")
-    ? console.log(`The word "${word}" is palindrome.`)
-    : console.log(`The word "${word}" is not palindrome.`);
+  isString(word)
+    ? word.toLowerCase() === invertString(word).toLowerCase()
+      ? console.log(`The word "${word}" is palindrome.`)
+      : console.log(`The word "${word}" is not palindrome.`)
+    : null;
 
 /////////////////////////////// END OF FUNCTIONS /////////////////////////////////
 
-console.clear();
 invertString("Hello World!"); //should output "!dlroW olleH"
 invertString(1234); //should throw an error as the argument is not a string
 countWords("hola mundo adios mundo", "mundo"); //should otput 2
 countWords("hola mundo adios mundo", 2); //should throw an error as both arguments need to be strings
+isPalindrome("salas"); //should output "The word "salas" is palindrome"
+isPalindrome("oso"); //should output "The word "salas" is palindrome"
