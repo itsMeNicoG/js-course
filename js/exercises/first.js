9c66d6e997c2efc425be3bd8270a0fd2cf7985ea
Nicolas Gongora <nicolasgongora28@gmail.com>
2021-01-02 13:04:16 -0500

    modify first set of exercises to use arrow functions

--- js/exercises/first.js
+++ js/exercises/first.js
@@ -18,19 +18,9 @@ countCharacters({ name: "Nico" }); //should output error "The value you provided
  *    2) Cut text to the desired length
  */
 
-function sliceText(text, newLength) {
-  try {
-    if (newLength > text.length) {
-      throw new Error("The new length has to be less than the original one.");
-    }
-    if (isString(text)) {
-      console.log(
-        `The string "${text}" has been cut to ${text.slice(0, newLength)}`
-      );
-    }
-  } catch (e) {
-    console.error(e);
-  }
+const sliceText = (text, newLength) => {
+    if (newLength > text.length) return console.warn("The new length has to be less than the original one.");
+    if (isString(text)) console.log(`The string "${text}" has been cut to ${text.slice(0, newLength)}`);
 }
 
 sliceText("Hola mundo", 4); //should output "Hola"
@@ -41,12 +31,10 @@ sliceText(1234, 4); //should throw error as the first parameter is not a string
  *    3) Separate string into array of words separated by the character provided
  */
 
-function splitText(text, separator) {
-  if (isString(text) && isString(separator)) {
-    console.log(`The string "${text}" has been transformed to: `);
-    console.log(text.split(separator));
-  }
-}
+const splitText= (text, separator) =>
+  isString(text) && isString(separator)
+    ?console.log(`The string "${text}" has been transformed to: `)
+    :console.log(text.split(separator));
 
 splitText("Hola mundo", " "); //should output [Hola,mundo]
 splitText("Hello my name is Nicolas", " "); //should output [Hello,my,name,is,Nicolas]
@@ -58,11 +46,10 @@ splitText("Hello World!", 1); //will output an error as the separator is not a s
  *    4) Repeat text a certain amount of times
  */
 
-function repeatText(text, repeatCount) {
-  if (isString(text) && isNumber(repeatCount)) {
-    console.log(text.repeat(repeatCount));
-  }
+const repeatText = (text, repeatCount) => {
+  if (isString(text) && isNumber(repeatCount)) console.log(text.repeat(repeatCount));
 }
+
 
 repeatText("Hello", 3); //will output HelloHelloHello
 repeatText("Hello", "a"); //will throw error as the number of repetitions is not a number
