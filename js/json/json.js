347a57b2ed260153f073085b78073bc8dcbfedf7
Nicolas Gongora <nicolasgongora28@gmail.com>
2021-01-19 16:24:59 -0500

    add more DOM examples

--- js/json/json.js
+++ js/json/json.js
@@ -3,44 +3,41 @@
 //JSON.parse will transform from string to JS objects
 //JSON.stringify will transform from js objects to string
 
-const json = {
-  "string": "Nico",
-  "number": "35",
-  "boolean": "true",
-  "array": "[]",
-  "object": {
-    
-  }
-}
-
-console.log(json);
-console.log(JSON.stringify(json));
-
-const jsonString = '{"string":"Nico","number":"35","boolean":"true","array":"[]","object":{}}';
-
-console.log(jsonString);
-console.log(JSON.parse(jsonString));
-
-let myJson;
-
 //get json data from API and print character names
 async function printCharacterNames() {
-  let jsondata = await fetch("https://breakingbadapi.com/api/characters").then(json => json.json()).then(data => myjson = data);
-  console.log(jsondata);
-  myJson = jsondata;
-  const iterator = myJson[Symbol.iterator]();
-  let next = iterator.next();
-  while (!next.done) {
-    console.log(`Current Character: ${next.value.name}`);
-    next = iterator.next();
-  }
-  
-  /*for (let character of myJson) {
-    console.log("Current character:" );
-    console.log(character.name);
-  }*/
+  let jsondata = await fetch(
+    "https://breakingbadapi.com/api/characters"
+  ).then((json) => json.json());
+  return jsondata;
 }
 
-printCharacterNames();
+async function init() {
+  const json = {
+    string: "Nico",
+    number: "35",
+    boolean: "true",
+    array: "[]",
+    object: {},
+  };
 
+  console.log(json);
+  console.log(JSON.stringify(json));
 
+  const jsonString =
+    '{"string":"Nico","number":"35","boolean":"true","array":"[]","object":{}}';
+
+  console.log(jsonString);
+  console.log(JSON.parse(jsonString));
+  console.groupCollapsed("Fetching information...");
+  const characters = await printCharacterNames().then((data) => data);
+  for (let character of characters) {
+    console.group(character.name);
+    for (let property in character) {
+      console.log(`${property}: ${character[property]}`);
+    }
+    console.groupEnd();
+  }
+  console.groupEnd();
+}
+
+init();
