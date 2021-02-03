78489e65e04a8c1b7eb0be57f7e819d0a2bec464
Nicolas Gongora <nicolasgongora28@gmail.com>
2021-02-03 08:33:36 -0500

    added 24hrs format

--- js/dom-exercises/clock.js
+++ js/dom-exercises/clock.js
@@ -1,6 +1,13 @@
+const options = {
+  hour12: false,
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+};
+
 const clock = () => {
   const clock = document.getElementById("current-time");
-  clock.innerHTML = new Date().toLocaleTimeString().toUpperCase();
+  clock.innerHTML = new Date().toLocaleTimeString("en-US", options);
 };
 
 const createClock = () => setInterval(clock, 1000);
