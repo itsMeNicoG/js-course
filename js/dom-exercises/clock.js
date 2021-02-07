bc8d4d319a43c6cfd5300be445277b7daf9bd5a8
Nicolas Gongora <nicolasgongora28@gmail.com>
2021-02-07 03:21:39 -0500

    reset alarm sound when stopped

--- js/dom-exercises/clock.js
+++ js/dom-exercises/clock.js
@@ -56,6 +56,7 @@ export function changeAlarmStatus($alarmBtn, $audio) {
     } else {
       $audio.pause();
       $alarmBtn.innerHTML = "Start Alarm";
+      $audio.currentTime = 0;
     }
   });
 }
