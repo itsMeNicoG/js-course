690d261e7f018678b986464b0ba6f9e3ddbbcb9d
Nicolas Gongora <nicolasgongora28@gmail.com>
2021-02-23 23:46:05 -0500

    change some styles on links

--- js/dom-exercises/responsive-js.js
+++ js/dom-exercises/responsive-js.js
@@ -1,8 +1,8 @@
 const map = `<iframe width="500" height="400" frameborder="0" src="https://www.bing.com/maps/embed?h=400&w=500&cp=45.44610078350057~-75.68505894776122&lvl=15&typ=d&sty=r&src=SHELL&FORM=MBEDV8" scrolling="no">
 </iframe>`;
 const video = `<iframe width="560" height="315" src="https://www.youtube.com/embed/zhl-Cs1-sG4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-const mapLink = `<a href="https://www.bing.com/maps?cp=45.44610078350057~-75.68505894776122&sty=r&lvl=15&FORM=MBEDLD">View Map Here!</a>`;
-const videoLink = `<a href="https://www.youtube.com/watch?v=zhl-Cs1-sG4">Watch Video Here!</a>`;
+const mapLink = `<a href="https://www.google.com/maps/@45.450407,-75.686767,15z?hl=en-US">VIEW MAP</a>`;
+const videoLink = `<a href="https://www.youtube.com/watch?v=zhl-Cs1-sG4">WATCH VIDEO</a>`;
 
 const beResponsive = ($section) => {
   $section.innerHTML = "";
